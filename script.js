337769e4adbb0a8095e72f3167dec9ce74ccc244
Sanu Roy <145701393+Sanu0910@users.noreply.github.com>
2025-03-11 20:03:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-gsap.from(".hero-text", {opacity: 0, y: -50, duration: 1, ease: "power2.out"});
-gsap.from(".project-card", {opacity: 0, y: 50, duration: 1, stagger: 0.2, ease: "power2.out"});
+gsap.from(".hero-text", { opacity: 0, y: -50, duration: 1, ease: "power2.out" })
+gsap.from(".project-card", {
+  opacity: 0,
+  y: 50,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power2.out",
+})
 
 VanillaTilt.init(document.querySelectorAll(".project-card"), {
-    max: 10,
-    speed: 300,
-    glare: true,
-    "max-glare": 0.2
-});
+  max: 10,
+  speed: 300,
+  glare: true,
+  "max-glare": 0.2,
+})
